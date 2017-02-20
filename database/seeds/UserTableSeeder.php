f38f367e466c62b458c672f87dc285449eb448d7
Pavlos Isaris <paulisaris@gmail.com>
2017-02-20 15:12:42 +0200

    Updated seed to include correct username

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -16,7 +16,7 @@ class UserTableSeeder extends Seeder
         DB::table('users')->insert(array(
             array('id'=> 1, 'first_name'=>'Paul', 'last_name'=> 'Isaris', 'state_id' => 1, 'email'=>'paul@scify.org', 'password' => '$2y$10$AB2Q2QgPWuMXyVis.EgUau2F2TZK25lFe6SB/LyEMFzL38Qqjgy1e'),
             array('id'=> 2, 'first_name'=>'SciFY', 'last_name'=> 'Tester', 'state_id' => 1, 'email'=>'test@scify.org', 'password' => '$2y$10$AB2Q2QgPWuMXyVis.EgUau2F2TZK25lFe6SB/LyEMFzL38Qqjgy1e'),
-            array('id'=> 3, 'first_name'=>'Alex', 'last_name'=> 'Tzoumas', 'state_id' => 1, 'email'=>'a.txoumas@scify.org', 'password' => '$2y$10$AB2Q2QgPWuMXyVis.EgUau2F2TZK25lFe6SB/LyEMFzL38Qqjgy1e')
+            array('id'=> 3, 'first_name'=>'Alex', 'last_name'=> 'Tzoumas', 'state_id' => 1, 'email'=>'a.tzoumas@scify.org', 'password' => '$2y$10$AB2Q2QgPWuMXyVis.EgUau2F2TZK25lFe6SB/LyEMFzL38Qqjgy1e')
         ));
     }
 }
