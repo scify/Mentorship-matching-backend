18c04c3881fcf2c1546c76338ff8f419ca589a56
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-03-03 15:43:58 +0200

    JP-178 created page to reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,76 +1,66 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+    <div class="bg-login printable">
+        <div class="login-screen">
+            <div class="panel-login blur-content">
+                <div class="panel-heading"><img src="{{asset('/assets/img/jobpairs_logo.png')}}" height="120" alt="">
+                </div><!--.panel-heading-->
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+                <div id="pane-forgot-password" class="panel-body active">
+                    <form class="form-horizontal forgotPasswordForm noInputStyles" role="form" method="POST" action="{{ url('/password/reset') }}">
                         {{ csrf_field() }}
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
+                        <h2>Reset your password</h2>
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="inputer">
+                                <div class="input-wrapper">
+                                    <input type="email" class="form-control" name="email" placeholder="Enter your email">
+                                </div>
                             </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="inputer">
+                                <div class="input-wrapper">
+                                    <input id="password" type="password" class="form-control" name="password" required placeholder="Enter your new password">
+                                </div>
                             </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">                            
+                            <div class="inputer">
+                                <div class="input-wrapper">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeholder="Re-enter your new password">
+                                </div>
                             </div>
+                            @if ($errors->has('password_confirmation'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                </span>
+                            @endif
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Reset Password
-                                </button>
-                            </div>
-                        </div>
+                        <div class="form-buttons clearfix">                            
+                            <button type="submit" class="btn btn-primary pull-right btn-ripple">Reset password</button>
+                        </div> 
                     </form>
                 </div>
             </div>
         </div>
+        <div class="bg-blur dark active">
+            <div class="overlay"></div><!--.overlay-->
+        </div><!--.bg-blur-->
     </div>
-</div>
 @endsection
