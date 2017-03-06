583394ee86a32991f3e018325c5b5a53ea2f8bcc
Pavlos Isaris <paulisaris@gmail.com>
2017-03-06 14:01:55 +0200

    When creating or updating user, account manager options are set only if the user is an account manager

--- app/BusinessLogicLayer/managers/UserManager.php
+++ app/BusinessLogicLayer/managers/UserManager.php
@@ -39,12 +39,18 @@ class UserManager {
         DB::transaction(function() use($newUser, $inputFields) {
             $newUser = $this->userStorage->saveUser($newUser);
             $this->userRoleManager->assignRolesToUser($newUser, $inputFields['user_roles']);
-            $this->handleCompanyAccountManager($newUser, $inputFields['company_id']);
-            if(isset($inputFields['capacity'])) {
-                $this->createOrUpdateAccountManagerCapacity($newUser->id, $inputFields['capacity']);
-            }
         });
+        if($newUser->isAccountManager()) {
+            $this->accountManagerDetails($newUser, $inputFields);
+        }
 
+    }
+
+    private function accountManagerDetails(User $user, array $inputFields) {
+        $this->handleCompanyAccountManager($user, $inputFields['company_id']);
+        if(isset($inputFields['capacity'])) {
+            $this->createOrUpdateAccountManagerCapacity($user->id, $inputFields['capacity']);
+        }
     }
 
     public function getAllUsers() {
@@ -76,10 +82,10 @@ class UserManager {
             $user = $this->userStorage->saveUser($user);
             $this->handleCompanyAccountManager($user, $inputFields['company_id']);
             $this->userRoleManager->editUserRoles($user, $inputFields['user_roles']);
-            if(isset($inputFields['capacity'])) {
-                $this->createOrUpdateAccountManagerCapacity($user->id, $inputFields['capacity']);
-            }
         });
+        if($user->isAccountManager()) {
+            $this->accountManagerDetails($user, $inputFields);
+        }
 
     }
 
