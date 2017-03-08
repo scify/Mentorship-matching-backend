7b6066f20bbf77d0af3f6fd4eed385680bfc018c
Pavlos Isaris <paulisaris@gmail.com>
2017-03-08 15:57:54 +0200

    Added seeder for mentor status lookup table

---
+++ database/seeds/MentorStatusLookupSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MentorStatusLookupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('mentor_status_lookup')->delete();
+        DB::table('mentor_status_lookup')->insert(array(
+            array('id'=> 1, 'status' => 'available', 'description' => 'Available'),
+            array('id'=> 2, 'status' => 'not_available_for_mentorship', 'description' => 'Not available for mentorship'),
+            array('id'=> 3, 'status' => 'matched', 'description' => 'Matched'),
+            array('id'=> 4, 'status' => 'black_listed', 'description' => 'Black listed'),
+        ));
+    }
+}
