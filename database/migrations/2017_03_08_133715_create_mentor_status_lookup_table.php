5530595ca78fd983f8029d6b64fc06598e00022d
Pavlos Isaris <paulisaris@gmail.com>
2017-03-08 15:57:17 +0200

    Mentor status lookup table migration

--- database/migrations/2017_03_08_133715_create_mentor_status_lookup_table.php
+++ database/migrations/2017_03_08_133715_create_mentor_status_lookup_table.php
@@ -13,17 +13,12 @@ class CreateMentorStatusLookupTable extends Migration
      */
     public function up()
     {
-        Schema::create('mentor_status_comment', function (Blueprint $table) {
+        Schema::create('mentor_status_lookup', function (Blueprint $table) {
             $table->integer('id')->unsigned();
             $table->primary('id');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('mentor_profile_id')->unsigned();
-            $table->foreign('mentor_profile_id')->references('id')->on('mentor_profile');
-            $table->text('text')->nullable();
-            $table->date('follow_up_date')->nullable();
+            $table->string('status');
+            $table->string('description');
             $table->softDeletes();
-            $table->timestamps();
         });
     }
 
