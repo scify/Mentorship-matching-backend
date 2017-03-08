2b2134046f6a0d54175430a138586aa0720ded91
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-03-08 17:47:37 +0200

    JP-188 displaying loader as shown in template when searching mentees

--- resources/assets/js/controllers/MenteesListController.js
+++ resources/assets/js/controllers/MenteesListController.js
@@ -10,8 +10,39 @@ window.MenteesListController.prototype = function () {
                 $('#deleteMenteeModal').find('input[name="mentee_id"]').val(menteeId);
             });
         },
-
-        init = function () {
+        searchBtnHandler = function () {
+            $("#searchBtn").on("click", function (e) {
+                getMenteesByFilter();
+            });
+        },
+        getMenteesByFilter = function() {
+            var data = {};
+            $.ajax({
+                method: "GET",
+                url: "byCriteria",
+                cache: false,
+                data: data,
+                beforeSend: function () {
+                    $('.panel-body').append('<div class="refresh-container"><div class="loading-bar indeterminate"></div></div>');
+                },
+                success: function (response) {
+                    $('.refresh-container').fadeOut(500, function() {
+                        $('.refresh-container').remove();
+                    });
+                    parseSuccessData(response);
+                },
+                error: function (xhr, status, errorThrown) {
+                    $('.refresh-container').fadeOut(500, function() {
+                        $('.refresh-container').remove();
+                    });
+                    console.log(xhr.responseText);
+                    $("#errorMsg").removeClass('hidden');
+                    //The message added to Response object in Controller can be retrieved as following.
+                    $("#errorMsg").html(errorThrown);
+                }
+            });
+        },
+        initHandlers = function() {
             deleteMenteeBtnHandler();
             $('a[data-toggle="tab"]').click(function (e) {
                 e.preventDefault();
@@ -22,6 +53,10 @@ window.MenteesListController.prototype = function () {
 
                 $(".card_" + $(this).attr('data-id')).find('div[id="' + $(this).attr('data-href') + '"]').addClass('active');
             });
+            searchBtnHandler();
+        },
+        init = function () {
+            initHandlers();
         };
     return {
         init: init
