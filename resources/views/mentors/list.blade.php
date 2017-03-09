8eafaef0136b79bcae26fa9b4f9c37efd50e5867
Pavlos Isaris <paulisaris@gmail.com>
2017-03-09 16:54:08 +0200

    JP-129 Improved single mentor view

--- resources/views/mentors/list.blade.php
+++ resources/views/mentors/list.blade.php
@@ -1,10 +1,4 @@
-<div class="row">
-    <h4 class="resultsTitle margin-bottom-20">{{$mentorViewModels->count()}} mentor(s) found</h4>
-
-        @foreach($mentorViewModels as $mentorViewModel)
-        <div class="col-md-12 padding-0">
-            @include('mentors.single', ['mentorViewModel' => $mentorViewModel])
-        </div>
-        @endforeach
-
-</div>
+<h4 class="resultsTitle margin-bottom-20">{{$mentorViewModels->count()}} mentor(s) found</h4>
+@foreach($mentorViewModels as $mentorViewModel)
+    @include('mentors.single', ['mentorViewModel' => $mentorViewModel])
+@endforeach
