5e34d333c73830b18a780264c34532bb3a70f59f
Pavlos Isaris <paulisaris@gmail.com>
2017-03-23 14:58:48 +0200

    Updated single mentor and mentee views to hide actions from non-admin users

--- resources/views/mentees/single.blade.php
+++ resources/views/mentees/single.blade.php
@@ -1,11 +1,13 @@
 <li class="has-action-left singleItem">
-    <a href="#"
-       data-toggle="modal"
-       data-userName="{{$menteeViewModel->mentee->first_name . $menteeViewModel->mentee->last_name}}"
-       data-menteeId="{{$menteeViewModel->mentee->id}}"
-       class="deleteMenteeBtn hidden"><i class="deleteIcon ion-android-delete"></i></a>
-    <a href="{{route('showEditMenteeForm', $menteeViewModel->mentee->id)}}" class="hidden secondItem"><i class="editIcon ion-edit"></i></a>
-    <a href="{{route('showMenteeProfilePage', $menteeViewModel->mentee->id)}}" class="visible">
+    @if(\Illuminate\Support\Facades\Auth::user()->userHasAccessToCRUDMentorsAndMentees())
+        <a href="#"
+           data-toggle="modal"
+           data-userName="{{$menteeViewModel->mentee->first_name . $menteeViewModel->mentee->last_name}}"
+           data-menteeId="{{$menteeViewModel->mentee->id}}"
+           class="deleteMenteeBtn hidden"><i class="deleteIcon ion-android-delete"></i></a>
+        <a href="{{route('showEditMenteeForm', $menteeViewModel->mentee->id)}}" class="hidden secondItem"><i class="editIcon ion-edit"></i></a>
+    @endif
+    <a href="{{route('showMenteeProfilePage', $menteeViewModel->mentee->id)}}" class="visible {{Illuminate\Support\Facades\Auth::user()->userHasAccessToCRUDMentorsAndMentees() ? '':'no-slide-left'}}">
         <div class="list-action-left">
             <img src="{{ asset("/assets/img/mentee_default.png") }}" class="face-radius" alt="">
         </div>
