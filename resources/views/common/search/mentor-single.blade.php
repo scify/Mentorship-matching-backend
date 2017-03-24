e07220ce30885d953f275261c0829f2880fd946b
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-03-24 11:22:43 +0200

    fixed generic search view to display results with the newly changed DB tables' columns

--- resources/views/common/search/mentor-single.blade.php
+++ resources/views/common/search/mentor-single.blade.php
@@ -4,6 +4,7 @@
     </div>
     <div class="list-content">
         <span class="title">{{ $mentor->first_name }} {{ $mentor->last_name }}</span>
-        <span class="caption">{{ $mentor->job_position }} - {{ $mentor->company_name }}</span>
+        <span class="caption">{{ $mentor->job_position }}@if($mentor->job_position != "" && $mentor->company != null) - @endif
+            @if($mentor->company != null){{ $mentor->company->name }}@endif</span>
     </div>
 </a>
