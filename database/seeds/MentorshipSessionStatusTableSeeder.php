5e13e76690713f2e00467a0b5dc625a26ae0bf0e
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-03-31 17:16:54 +0300

    JP-245 added extra statuses in mentorship sessions

--- database/seeds/MentorshipSessionStatusTableSeeder.php
+++ database/seeds/MentorshipSessionStatusTableSeeder.php
@@ -14,15 +14,17 @@ class MentorshipSessionStatusTableSeeder extends Seeder
     {
         DB::table('mentorship_session_status_lookup')->delete();
         DB::table('mentorship_session_status_lookup')->insert(array(
-            array('id'=> 1, 'title' => 'available_mentee', 'description' => 'Mentee is available'),
-            array('id'=> 2, 'title' => 'available_mentor', 'description' => 'Mentor is available'),
-            array('id'=> 3, 'title' => 'started', 'description' => 'Started - Material Sent'),
-            array('id'=> 4, 'title' => 'first_meeting', 'description' => '1st meeting completed'),
-            array('id'=> 5, 'title' => 'second_meeting', 'description' => '2nd meeting completed'),
-            array('id'=> 6, 'title' => 'third_meeting', 'description' => '3rd meeting completed'),
-            array('id'=> 7, 'title' => 'fourth_meeting', 'description' => '4th meeting completed'),
-            array('id'=> 8, 'title' => 'evaluation_sent', 'description' => 'Completed - Evaluation sent'),
-            array('id'=> 9, 'title' => 'follow_up_sent', 'description' => 'Completed - Follow up sent'),
+            array('id'=> 1, 'title' => 'pending', 'description' => 'Pending'),
+            array('id'=> 2, 'title' => 'available_mentee', 'description' => 'Mentee is available'),
+            array('id'=> 3, 'title' => 'available_mentor', 'description' => 'Mentor is available'),
+            array('id'=> 4, 'title' => 'started', 'description' => 'Started - Material Sent'),
+            array('id'=> 5, 'title' => 'first_meeting', 'description' => '1st meeting completed'),
+            array('id'=> 6, 'title' => 'second_meeting', 'description' => '2nd meeting completed'),
+            array('id'=> 7, 'title' => 'third_meeting', 'description' => '3rd meeting completed'),
+            array('id'=> 8, 'title' => 'fourth_meeting', 'description' => '4th meeting completed'),
+            array('id'=> 9, 'title' => 'evaluation_sent', 'description' => 'Completed - Evaluation sent'),
+            array('id'=> 10, 'title' => 'follow_up_sent', 'description' => 'Completed - Follow up sent'),
+            array('id'=> 11, 'title' => 'cancelled', 'description' => 'Cancelled'),
         ));
     }
 }
