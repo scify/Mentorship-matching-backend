9096fb7925cc3451debe77c0f50da11e458ca3a4
Pavlos Isaris <paulisaris@gmail.com>
2017-04-03 10:04:33 +0300

    Added menu items for admin that is also a matcher or an account manager

--- resources/views/common/menu_items/account_manager_menu_items.blade.php
+++ resources/views/common/menu_items/account_manager_menu_items.blade.php
@@ -11,3 +11,8 @@
 <li class="{{ (Route::current()->getName() == 'showMentorshipSessionsForAccountManager') ? 'open' : '' }}">
     <a href="{{ route('showMentorshipSessionsForAccountManager') }}"> My Mentorship Sessions </a>
 </li>
+@if($user->isMatcher())
+    <li class="{{ (Route::current()->getName() == 'showMatchesForMatcher') ? 'open' : '' }}">
+        <a href="{{ route('showMatchesForMatcher', $user->id) }}">My Matches </a>
+    </li>
+@endif
--- resources/views/common/menu_items/matcher_menu_items.blade.php
+++ resources/views/common/menu_items/matcher_menu_items.blade.php
@@ -5,8 +5,13 @@
     <a href="{{ route('showAllMentees') }}">Mentees </a>
 </li>
 <li class="{{ (Route::current()->getName() == 'showMatchesForMatcher') ? 'open' : '' }}">
-    <a href="{{ route('showMatchesForMatcher', \Illuminate\Support\Facades\Auth::user()->id) }}">My Matches </a>
+    <a href="{{ route('showMatchesForMatcher', $user->id) }}">My Matches </a>
 </li>
 <li class="{{ (Route::current()->getName() == 'showAllMentorshipSessions') ? 'open' : '' }}">
     <a href="{{ route('showAllMentorshipSessions') }}"> Mentorship Sessions </a>
 </li>
+@if($user->isAccountManager())
+    <li class="{{ (Route::current()->getName() == 'showMentorshipSessionsForAccountManager') ? 'open' : '' }}">
+        <a href="{{ route('showMentorshipSessionsForAccountManager') }}"> My Mentorship Sessions </a>
+    </li>
+@endif
