7f84d551b39d9d8756f67607dd9983a5b2461339
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-04-10 16:46:14 +0300

    JP-247 set up email common view and changed register mail a little

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,28 +1,18 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <title>Job Pairs</title>
-        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-        <meta name="apple-touch-fullscreen" content="yes">
-    </head>
-    <body>
-        <h1 style="text-align: center; font-size: 16px;">Job Pairs</h1>
-        <br>
-        <p style="text-align: center; font-size: 14px;">Your account at Job Pairs BackOffice has been created</p>
-        <br>
-        <p style="text-align: center; font-size: 14px;">You can login here: 
-            <a href="{{ url('/login') }}">{{ url("/login") }}</a>
-        </p>
-        <br>
-        <p style="text-align: center; font-size: 14px;">email: 
-            <span style="display: inline-block;">{{ $email }}</span>
-        </p>
-        <p style="text-align: center; font-size: 14px;">password: 
-            <span style="display: inline-block;">{{ $password }}</span>
-        </p>
-    </body>
-</html>
+@extends('emails.common')
+@section('email-body')
+    <h1 style="text-align: center; font-size: 26px; margin-top: 15px; margin-bottom: 10px;">Job Pairs</h1>
+    <p style="text-align: center;">Your account at Job Pairs BackOffice has been created</p>
+    <p style="text-align: center;">You can login here:
+        <a href="{{ url('/login') }}">{{ url("/login") }}</a>
+    </p>
+    <div style="display: table; margin-left: auto; margin-right: auto; margin-top: 40px; margin-bottom: 40px;">
+        <div style="display: table-row;">
+            <p style="text-align: right; font-size: 16px; font-weight: bold; display: table-cell; padding-right: 10px;">email:</p>
+            <p style="display: table-cell; color: #555;">{{ $email }}</p>
+        </div>
+        <div style="display: table-row;">
+            <p style="text-align: right; font-size: 16px; font-weight: bold; display: table-cell; padding-right: 10px;">password:</p>
+            <p style="display: table-row; color: #555;">{{ $password }}</p>
+        </div>
+    </div>
+@endsection
