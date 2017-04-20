518376474191c000e34a39573f1c9ed5b1622305
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-04-20 12:56:21 +0300

    fixed bug on mentors list delete mentor action

--- resources/views/mentors/single-mentor-menu.blade.php
+++ resources/views/mentors/single-mentor-menu.blade.php
@@ -14,7 +14,7 @@
         <a href="javascript:void(0)"
            data-toggle="modal"
            data-userName="{{$mentorViewModel->mentor->first_name . " " . $mentorViewModel->mentor->last_name}}"
-           data-menteeId="{{$mentorViewModel->mentor->id}}"
+           data-mentorId="{{$mentorViewModel->mentor->id}}"
            class="deleteMentorBtn hidden menu-action"><i class="deleteIcon ion-android-delete"></i></a>
         <a href="{{route('showEditMentorForm', $mentorViewModel->mentor->id)}}" class="hidden secondItem menu-action"><i class="editIcon ion-edit"></i></a>
     @elseif($loggedInUser->userHasAccessOnlyToChangeAvailabilityStatusForMentorsAndMentees())
