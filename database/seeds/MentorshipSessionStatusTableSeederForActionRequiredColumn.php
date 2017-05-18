5adcc9d08a8ec71fb6ca602259261b1b568b2a47
Pavlos Isaris <paulisaris@gmail.com>
2017-05-18 12:28:50 +0300

    Added seeder for action_required column on mentorship session status table

---
+++ database/seeds/MentorshipSessionStatusTableSeederForActionRequiredColumn.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MentorshipSessionStatusTableSeederForActionRequiredColumn extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('mentorship_session_status_lookup')
+            ->where('id', 1)
+            ->update(['action_required' => 'Confirm availability for mentee and mentor. Clicking the button below will send first an email to mentee (asking for his/her availability) and then to mentor.']);
+
+        DB::table('mentorship_session_status_lookup')
+            ->where('id', 4)
+            ->update(['action_required' => 'Both mentor and mentee are available. You should send an introduction email between the 2 parties. If you have already done it, update the status below to "Introduction email & related info"']);
+
+    }
+}
