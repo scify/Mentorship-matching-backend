be92fd21141fd783f44ae9ee0cb27089908b4986
Pavlos Isaris <paulisaris@gmail.com>
2017-05-18 12:02:20 +0300

    JP-352 JP-354 JP-355 Updated descriptions for mentorship session status table

---
+++ database/seeds/MentorshipSessionStatusTableSeederPatch.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MentorshipSessionStatusTableSeederPatch extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('mentorship_session_status_lookup')
+            ->where('id', 1)
+            ->update(['description' => 'Assigned to Account Manager']);
+
+        DB::table('mentorship_session_status_lookup')
+            ->where('id', 2)
+            ->update(['description' => 'Emailed Mentee and Mentor to confirm availability']);
+
+        DB::table('mentorship_session_status_lookup')
+            ->where('id', 5)
+            ->update(['description' => 'Introduction email & related info']);
+    }
+}
