310999aece904fd34cf1549cbf7492be456212e6
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-06-09 17:22:19 +0300

    updated text to mentor status reactivation email

--- app/Notifications/MentorStatusReactivation.php
+++ app/Notifications/MentorStatusReactivation.php
@@ -42,11 +42,11 @@ class MentorStatusReactivation extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject("Job-Pairs | Η συνεδρία σας ολοκληρώθηκε!")
+            ->subject("Job-Pairs | Θα θέλατε να συμμετέχετε ξανά;")
             ->greeting('Αγαπητέ mentor,')
-            ->line('Συγχαρητήρια!')
-            ->line('<div style="margin-top: 1em; color: #74787E; font-size: 16px; line-height: 1.5em;">Θα θέλαμε να σας ενημερώσουμε πως μόλις ολοκληρώθηκε μια συνεδρία στην οποία συμμετείχατε.</span>')
-            ->line('<div style="margin-top: 1em; color: #74787E; font-size: 16px; line-height: 1.5em;">Μπορείτε εάν το επιθυμείτε να θέσετε το προφίλ σας διαθέσιμο για νέα συνεδρία κάνοντας κλικ στο παρακάτω σύνδεσμο.</span>')
+            ->line('Συγχαρητήρια για την ολοκλήρωση των συναντήσεων σας!')
+            ->line('<div style="margin-top: 1em; color: #74787E; font-size: 16px; line-height: 1.5em;">Θα ήταν μεγάλη μας χαρά να λάβετε ξανά μέρος και να σας φέρουμε σε επαφή με νέο mentee.</span>')
+            ->line('<div style="margin-top: 1em; color: #74787E; font-size: 16px; line-height: 1.5em;">Αν το επιθυμείτε, μπορείτε να δηλώσετε διαθέσιμος για νέο κύκλο συναντήσεων κάνοντας κλικ στον παρακάτω σύνδεσμο.</span>')
             ->action('Επιθυμώ νέα συνεδρία', route('setMentorStatusAvailable', ['id' => $this->mentor->id, 'email' => $this->mentor->email, 'lang' => 'gr']))
             ->line('<div style="margin-top: 1em; color: #74787E; font-size: 16px; line-height: 1.5em;">Με εξαιρετική εκτίμηση,</div>')
             ->line('Η ομάδα του Job-Pairs')->to($notifiable->routeNotificationFor('mail'));
