4ef097bcf3aaaccdc856f5257013661f22e98471
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-06-28 15:56:32 +0300

    added better comment in migration that adds status_id column to the specialty table

--- database/migrations/2017_06_28_111218_add_status_to_specialty_table.php
+++ database/migrations/2017_06_28_111218_add_status_to_specialty_table.php
@@ -14,12 +14,9 @@ class AddStatusToSpecialtyTable extends Migration
     public function up()
     {
         Schema::table('specialty', function (Blueprint $table) {
+            // BE CAREFUL!!! You need to seed `specialty_status` table
+            // before continuing with this migration.
             $table->integer('status_id')->unsigned()->after('name')->default(1);
-            // BE CAREFUL!!! you might need to comment below line and `$table->dropForeign(['status_id']);`
-            // from down() function as well in order to get it working. This is caused because the seeder
-            // hasn't been run yet. Drop the `status_id` column from the `specialty` table, run the seeder
-            // by doing `php artisan db:seed --class=SpecialtyStatusTableSeeder` and then create manually
-            // the foreign key to the `specialty_status` table.
             $table->foreign('status_id')->references('id')->on('specialty_status');
         });
     }
