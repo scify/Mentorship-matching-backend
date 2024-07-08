94b23dc67aa817760ee4c06f04d9f7d90d97aba5
Paul Isaris <paulisaris@gmail.com>
2024-07-08 10:31:15 +0300

    ifs

--- app/BusinessLogicLayer/managers/MentorshipSessionManager.php
+++ app/BusinessLogicLayer/managers/MentorshipSessionManager.php
@@ -620,13 +620,13 @@ class MentorshipSessionManager {
         if ($role === 'mentee') {
             $invitedPerson = $mentorshipSession->mentee;
             // this is the case when the mentee is available
-            if ($mentorshipSession->status_id === MentorshipSessionStatuses::$statuses['introduction_sent'])
+            // if ($mentorshipSession->status_id === MentorshipSessionStatuses::$statuses['introduction_sent'])
                 $statusToSet = MentorshipSessionStatuses::$statuses['available_mentee'];
 
         } else if ($role === 'mentor') {
             $invitedPerson = $mentorshipSession->mentor;
             // this is the case when mentor is available
-            if ($mentorshipSession->status_id === MentorshipSessionStatuses::$statuses['available_mentee'])
+            // if ($mentorshipSession->status_id === MentorshipSessionStatuses::$statuses['available_mentee'])
                 $statusToSet = MentorshipSessionStatuses::$statuses['available_mentor'];
         }
         if ($statusToSet !== -1 && $invitedPerson->id == $id && $invitedPerson->email === $email) {
